2aeb2c14b3ad79af9e20a28d2accb4b36e72a1e0
Sergey <31410003+Pyanykh@users.noreply.github.com>
2017-09-22 00:56:01 +0300

    Add files via upload

---
+++ src/main/java/com.vertex.java.homework1/Teacher.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Teacher {
+    private ArrayList<Marker> list = new ArrayList<>();
+
+    public void takeMarker(Marker marker) {
+        if (marker.getTeacher() == null) {
+            list.add(marker);
+            marker.setTeacher(this);
+        } else System.out.println("Этот маркер у другого преподавателя");
+    }
+
+    public void write() {
+        System.out.print("Введите текст:");
+        Scanner scan = new Scanner(System.in);
+        write(scan.next());
+    }
+
+    public void write(String s) {
+        if (!list.isEmpty()) {
+            for (int n = 0; n < s.length(); n++) {
+                for (Marker aList : list) {
+                    if (aList.getVolume() != 0 && n < s.length()) {
+                        System.out.print("<" + aList.getColor() + ">");
+                        while (aList.getVolume() != 0 && n < s.length()) {
+                            System.out.print(s.charAt(n));
+                            n++;
+                            aList.setVolume(aList.getVolume() - 1);
+                        }
+                        System.out.print("</" + aList.getColor() + ">");
+                    }
+                }
+            }
+        } else System.out.println("Нечем писать");
+    }
+}
