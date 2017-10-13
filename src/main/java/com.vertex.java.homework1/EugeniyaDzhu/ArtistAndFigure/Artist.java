c8c873aa143f12439fcd76cd3b36a17012c5e5c2
Eugeniya Kulenich <eugeniya.dzhu@gmail.com>
2017-10-13 18:12:20 +0300

    Home work "Artist with figures". Add classes "Circle", "Figure", "Oval", "Point", "Rectangular", "Square".

--- src/main/java/com.vertex.java.homework1/EugeniyaDzhu/ArtistAndFigure/Artist.java
+++ src/main/java/com.vertex.java.homework1/EugeniyaDzhu/ArtistAndFigure/Artist.java
@@ -2,8 +2,8 @@ package com.vertex.java.homework1.EugeniyaDzhu.ArtistAndFigure;
 
 import com.vertex.java.homework1.EugeniyaDzhu.ArtistAndFigure.FigureArrey;
 
-public enum Artist {
-    ARTIST {Figure[] figureArrey(){ return  FigureArrey.getFigureArrey();}
+public class Artist {
 
+    Figure[] figures = new Figure[100]
     }
 }
